58cdbf65caa53e69e3aaa8b4ae30b8b12e826670
aliyu <1324149139@qq.com>
2022-10-17 13:10:46 +0800

    feature:创建欢迎界面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,6 +1,23 @@
+import { RouteRecordRaw } from 'vue-router'
+import { First } from '../components/First'
+import { Fourth } from '../components/Fourth'
+import { Second } from '../components/Second'
+import { Third } from '../components/Third'
 import { Bar } from '../views/Bar'
 import { Foo } from '../views/Foo'
-export const routes = [
+import { Welcome } from '../views/Welcome'
+export const routes: RouteRecordRaw[] = [
   { path: '/', component: Foo },
   { path: '/about', component: Bar },
+  {
+    path: '/welcome',
+    component: Welcome,
+    children: [
+      { path: '1', component: First },
+      { path: '2', component: Second },
+      { path: '3', component: Third },
+      { path: '4', component: Fourth }
+
+    ]
+  }
 ]
